84a8b52ac1962ba2619fecb6cc08119c9a8f521a
Kokosalah45 <kokp.amged@gmail.com>
2022-08-29 20:33:36 +0200

    added https instead of http to the baseURL

--- api/apiClient.js
+++ api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://pokemon-rest-api.herokuapp.com",
+  baseURL: "https://pokemon-rest-api.herokuapp.com",
   headers: {
     Accept: "application/json",
     "content-type": "application/json",
