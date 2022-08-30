26a5fcc53509376169e94477e7bde64061a249c7
Kokosalah45 <kokp.amged@gmail.com>
2022-08-30 13:12:45 +0200

    fixed paginate logic pretty much all of it and create the paginate page

--- components/Paginate.jsx
+++ components/Paginate.jsx
@@ -1,30 +1,22 @@
 import React from "react";
+import Image from "next/image";
 import { useQuery } from "@tanstack/react-query";
 import { useState, useEffect } from "react";
 import { getPokemonData } from "../api/pokemon";
+import shortid from "shortid";
+import { ImSpinner8 } from "react-icons/im";
+import { MdNavigateNext, MdNavigateBefore } from "react-icons/md";
 export default function Paginate({ pageSize, pageCount }) {
-  const [currentPage, setCurrentPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(0);
   const [noOfPages, setNoOfPages] = useState(0);
-  const [take, setTake] = useState(0);
-  const [cursor, setCursor] = useState(0);
+
   useEffect(() => {
-    setNoOfPages(Math.ceil(pageCount / pageSize));
+    setNoOfPages(Math.floor(pageCount / pageSize));
     // eslint-disable-next-line
   }, []);
 
-  useEffect(() => {
-    const tempTake = currentPage * pageSize;
-    setTake(tempTake);
-    setCursor(tempTake - pageSize);
-    // eslint-disable-next-line
-  }, [currentPage]);
-
-  const { data, isLoading } = useQuery(
-    ["page", cursor, take],
-    () => getPokemonData(cursor, take),
-    {
-      enabled: !!take,
-    }
+  const { data, isLoading } = useQuery(["page", currentPage + 1], () =>
+    getPokemonData(currentPage * pageSize, pageSize)
   );
   const next = () => {
     setCurrentPage(currentPage + 1);
@@ -34,17 +26,77 @@ export default function Paginate({ pageSize, pageCount }) {
   };
   return (
     <>
-      <div>{currentPage}</div>
-      <button onClick={prev} disabled={!cursor}>
-        prev
-      </button>
-      <button onClick={next} disabled={currentPage === noOfPages}>
-        next
-      </button>
-      <div>{cursor}</div>
-      <div>{take}</div>
+      <div className="overflow-x-auto w-dynamic m-auto min-h-screen relative  font-mono  ">
+        <div className="flex  justify-around border-2  mb-4 p-3 rounded-md my-4 ">
+          <button onClick={prev} disabled={!currentPage}>
+            <MdNavigateBefore className="w-8 h-8 rounded-[50%] border-2 text-white cursor-pointer hover:text-black hover:bg-white transition-all" />
+          </button>
+          <h2 className="text-3xl font-bold text-white">{currentPage + 1}</h2>
+          <button onClick={next} disabled={currentPage === noOfPages}>
+            <MdNavigateNext className="w-8 h-8 rounded-[50%] border-2 text-white cursor-pointer hover:text-black hover:bg-white transition-all" />
+          </button>
+        </div>
 
-      <div>{JSON.stringify(data)}</div>
+        <table className=" w-full text-white  table-fixed rounded-lg overflow-hidden capitalize text-lg text-center ">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="py-3 px-6">
+                Pokemon Name
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Image
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Voted for
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Voted against
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {!data ? (
+              <tr className="min-h-screen w-full relative">
+                <td className="h-screen " colSpan="3"></td>
+                <div className="absolute inset-0 grid place-items-center dark:bg-gray-700">
+                  <ImSpinner8 className="animate-spin w-14 h-14" />
+                </div>
+              </tr>
+            ) : (
+              data?.results?.map(
+                ({ name, img_url, voted_for, voted_against }) => (
+                  <tr
+                    className="bg-white border-b dark:bg-gray-800 dark:border-gray-700  cursor-pointer hover:bg-green-500 select-none"
+                    key={shortid.generate()}
+                  >
+                    <th
+                      scope="row"
+                      className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      {name}
+                    </th>
+                    <td className="py-4 px-6">
+                      <Image
+                        src={img_url}
+                        layout="fixed"
+                        width={80}
+                        height={80}
+                        alt={`image of ${name} pokemon`}
+                      />
+                    </td>
+                    <td className="py-4 px-6">{voted_for}</td>
+                    <td className="py-4 px-6">{voted_against}</td>
+                  </tr>
+                )
+              )
+            )}
+          </tbody>
+        </table>
+      </div>
+
+      <button onClick={() => setCurrentPage(noOfPages)}>
+        set to last page
+      </button>
     </>
   );
 }
